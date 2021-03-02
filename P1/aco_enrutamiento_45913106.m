e36800e3196d23ddb7b233948059e4209c112b87
ivvasel <ivvasel@teleco.upv.es>
2021-03-02 01:27:24 +0100

    Mejores explicaciones en el 2

--- P1/aco_enrutamiento_45913106.m
+++ P1/aco_enrutamiento_45913106.m
@@ -80,66 +80,67 @@ for n=1:nants %Recorre todas las hormigas
     
     %%No ha llegado a la comida y sigue buscando
     else
-        auxNei=model.nei{pos};
-        checkNei=auxNei;
-        allP=[];
-        %%calcula tau y eta de la posicion con los posibles vecinos 
-        %%que no se hayan visitado ya. 
-        %%Calculamos tmb probabilidad de los posibles casos por separado
-        for p=1:length(auxNei);  
+        auxNei=model.nei{pos};%Los vecinos en la posicion de la hormiga
+        checkNei=auxNei;%Almacena los vecinos en una variable variante
+        allP=[];%Inicializa el vector de todas las prob entre la hormiga y los vecinos
         
+        %%calcula tau y eta de la posicion con los posibles vecinos que no se hayan visitado ya. 
+        %%Obtenemos el vector con las probabilidades de saltar a cada
+        %%posible vecino
         
-        posiblesalto=auxNei(p); %Obtiene un posible salto de model.nei{p}
-        comprobar=find(antPath==posiblesalto); %Comprobar si posiblesalto ya se encuentra en el camino de la ant
-        if isempty(comprobar)
-            %Calculamos p hacia el posiblesalto
-            auxTau=tau(pos,posiblesalto);
-            auxEta=eta(pos,posiblesalto);            
-            allP=[allP (auxTau^alpha)*(auxEta^beta)];        
+        for p=1:length(auxNei) %Indices de la matrix auxiliar de vecinos        
+            posiblesalto=auxNei(p); %Obtiene un posible salto de model.nei{p}=auxNei(p)
+            comprobar=find(antPath==posiblesalto); %Comprobar si posiblesalto ya se encuentra en el camino de la ant
+            if isempty(comprobar) %Comprueba si se ha devuelto algún indice indicando que por ese nodo ya ha pasado la hormiga
+                %Si no hay indice indica que la hormiga no ha pasado por
+                %ese nodo
+                %Calculamos probabilidad hacia el posiblesalto
+                TauVecino=tau(pos,posiblesalto);%Obtenemos la feromona del camino hacia el nodo vecino
+                EtaVecino=eta(pos,posiblesalto);%Obtenemos la visibilidad del nodo vecino         
+                allP=[allP (TauVecino^alpha)*(EtaVecino^beta)]; %Vecotr que almacena los valores del sumatorio del denominador de la formula de pij       
         
-        else
-            elimnei=find(checkNei==posiblesalto);
-            checkNei(elimnei,:)=[];   
-            
-        end 
+            else %Si la hormiga ha pasado ya por el posible salto entonces:
+                elimNei=find(checkNei==posiblesalto); %Obtiene el indice donde se encuentra el nodo que se desea eliminar de posibles saltos
+                checkNei(elimNei,:)=[]; %Elimina el nodo del vecto. Se utiliza una variable extra para no interferir en el for que utiliza auxNei.            
+            end 
         
         end
     
-    
-    
-        %%Sumamos la probabilidad de los posibles casos
-        %calcula tau y eta de la posicion con los posibles vecinos que no se hayan visitado ya
-        sumaP = sum(allP);
-        P=[];
+        sumaP = sum(allP); %Sumatorio del denominador de la formula pij
+        P=[]; %Inicializa el vector P donde se almacenarán las probabilidades del cumsum
     
     
         %%Calculamos la probabilidad para cada caso y creamos el vector P
-        for p=1:length(allP);
-        P=[P allP(p)/sumaP];
+        for p=1:length(allP) %Recorre todo el vector sumaP que indica cuantas probabilidades se han calculado y por tanto cuantos posibles vecinos hay
+        P=[P allP(p)/sumaP]; %Almacena las probabilidades pij
         end
     
     
-        %%Elegimos de forma euristica el nodo al que iremos
+        %%Elegimos de forma heuristica el nodo al que iremos
         r=rand;
         C=cumsum(P);
-        j=find(r<=C,1,'first'); %selecciona el primero mayor que r
+        j=find(r<=C,1,'first'); %selecciona el primero mayor que r y devuelve el indice
+        %%Elige un posible salto entre todos y se añade al vector de nodos
+        %%recorridos por la hormiga
         ant(n).Tabu=[antPath checkNei(j)];%introduce en el path el nodo seleccionado
     
         %%Comprueba si no hay vecinos a los que ir y reinicia el path
+        %%Exactamente comprueba si se han eliminado todos los posibles
+        %%saltos en el proceso anterior, esto indicará que el nodo no ha
+        %%encontrado un camino por donde avanzar.
         if isempty(checkNei)
-            antPath=[nest];
-            ant(n).Tabu=antPath;            
+            antPath=[nest]; %Devuelve a la hormiga al nest
+            ant(n).Tabu=antPath; %Reinicia la variable 
+            continue %Salta a la siguiente hormiga
         end
         
         %%Comprueba si ha llegado la hormiga a la comida
-        antPath=ant(n).Tabu;
-        pos=antPath(length(antPath));
-        if pos==food
-            exitosas=exitosas+1;
-            %Añade el coste de la ruta a la matriz
-            %ant(n).Cost=length(antPath);
-            if exitosas==nants
-                exito=true;
+        antPath=ant(n).Tabu; %Actualiza la varible con el avance de la hormiga incluido
+        pos=antPath(length(antPath));%Actualiza la variable con el nodo actual de la hormiga
+        if pos==food %Compara la el nodo de la hormiga con el de la comida
+            exitosas=exitosas+1; %Si encuentra la comida suma +1 a las hormiga que han logrado llegar a la comida
+            if exitosas==nants %Comprueba si todas las hormigas han llegado
+                exito=true; %En de llegar todas se cambia el valor de la booleana exito
                 
             end
         end 
@@ -150,38 +151,39 @@ end
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         % UPDATE PHEROMONES
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        sumCoste=[];
-        for p=1:nants
-            antPath=ant(p).Tabu;
-            coste=[ant(p).Cost];%Inicializamos el coste con el valor almacenado
+        sumCoste=[];%Inicializamos el sumatorio de los costes
+        for p=1:nants %Recorre todas las hormigas
+            antPath=ant(p).Tabu;%Obtenemos el camino que ha llevado la hormiga
+            coste=[ant(p).Cost];%Inicializamos el coste con el valor almacenado. Aquí se almacenarán los costes de cada salto para una única hormiga(inicializado en vacio)
             
             %For para almacenar la distancia entre todos los saltos
             for l=2:length(antPath)
-                nodoi=antPath(l-1);
-                nodoj=antPath(l);
-                coste=[coste model.D(nodoi,nodoj)]; %Añadimos las distancias al vector
+                nodoi=antPath(l-1);%Obtenemos el nodo i
+                nodoj=antPath(l);%Obtenemos el nodo j
+                coste=[coste model.D(nodoi,nodoj)]; %Añadimos la distancia entre cada salto del camino seguido por la hormiga
                 
             end 
-            coste=sum(coste); %Sumamos las distancias del vector de una hormiga
+            
+            coste=sum(coste); %Sumamos las distancias del vector de una hormiga y obtenemos la distancia total recorrida
             ant(p).Cost=coste; %Almacenamos la distancia de una hormiga
-            sumCoste=[sumCoste coste ];%Almacenamos en un vector aux el coste de todas las hormigas
-            
-            
+            sumCoste=[sumCoste coste ];%Almacenamos en un vector aux el coste de todas las hormigas. Lo utilizaremos para encontrar el minimo valor entre todas las hormigas
+
         end
         
-        betterant = find(sumCoste==min(sumCoste));%Apunta a la hormiga con el camino mas corto
-        betterpath = ant(betterant).Tabu;
-        varFijatau=mrdivide(Q,ant(betterant(1)).Cost);
-        %x = mrdivide(B,A) forma alternativa de hacer div
-        for l=2:length(betterpath)
-                nodoi=betterpath(l-1);
-                nodoj=betterpath(l);
-                tau(nodoi,nodoj)=tau(nodoi,nodoj)+varFijatau; %Añadimos las distancias al vector
+        betterant = find(sumCoste==min(sumCoste)); %Apunta a la hormiga con el camino mas corto
+        betterpath = ant(betterant).Tabu; %Obtiene el camino de la hormiga con menor coste
+        varFijatau=mrdivide(Q,ant(betterant(1)).Cost);%Calculo la variable que es constante. Apunta al índice 1 ya que puede ser que varias hormigas tengan el mismo coste, por ranto se elige la primera hormiga
+        %x = mrdivide(B,A) forma alternativa de hacer div (utilizada porque
+        %pensaba que la habitual fallaba :=) )
+        for l=2:length(betterpath)%Recorre los indices del mejor camino
+                nodoi=betterpath(l-1); %Nodosi
+                nodoj=betterpath(l); %Nodosj
+                tau(nodoi,nodoj)=tau(nodoi,nodoj)+varFijatau; %Actualizamos las distancias en la matriz de feromonas
                 
         end 
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        tau=(1-rho)*tau;
+        tau=(1-rho)*tau; %Evaporamos una parte de todos los caminos
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%
         
         
